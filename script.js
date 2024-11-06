dad508641c074c2c2a3c2d781b226eb12015b2c9
Joicemotamoreira <joicemotamoreira@live.com>
2024-11-06 16:28:02 +0000

    Created menu toggle

---
+++ script.js
@@ -0,0 +1,9 @@
+function menuToggle() {
+  let newMenu = document.querySelector(".newMenu");
+  newMenu.classList.add("active");
+}
+
+function closeNewMenu() {
+  let newMenu = document.querySelector(".newMenu");
+  newMenu.classList.remove("active");
+}
